2131a29ae38ebc87309baa54ca5ffb6449732179
Erik Schnetter <schnetter@gmail.com>
2022-09-06 15:35:27 -0400

    Activate CI

--- src/SixelTerm.jl
+++ src/SixelTerm.jl
@@ -12,19 +12,22 @@ function Base.display(d::SixelDisplay, m::MIME{Symbol("image/png")}, x)
     s = Stream(format"six", io, "t.six")
     ImageMagick.save(s, im)
     write(stdout, take!(io))
+    return nothing
 end
 
-function Base.display(d::SixelDisplay, x) 
+function Base.display(d::SixelDisplay, x)
     if showable("image/png", x)
         display(d, "image/png", x)
     else
         # fall through to the Displays lower in the display stack
         throw(MethodError(display, "nope"))
     end
+    return nothing
 end
 
 function __init__()
     Base.Multimedia.pushdisplay(SixelDisplay())
+    return nothing
 end
-    
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,9 @@
-using Test
+using CairoMakie
 using SixelTerm
+using Test
+
+@testset "Display a PNG image" begin
+    v = rand(10, 2)
+    scene = scatter((@view v[:, 1]), (@view v[:, 2]); markersize=30.0)
+    display(scene)
+end
